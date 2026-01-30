848751cd4126043330844cff12f0ef6b680128a2
Charles Nii Adotey Allotey <charlesallotey1995@gmail.com>
2026-01-30 08:15:34 +0000

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: ['@iconify/utils']
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
